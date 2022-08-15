60dc594695274ea40369abc314c9ebdce09dac92
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-15 11:10:16 +0200

    add list component

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeple] = useState(data)
+  return <main>
+    <section className='container'>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people} />
+      <button onClick={() => setPeple([])}>clear all</button>
+    </section>
+  </main>;
 }
 
 export default App;
