26f68230e3dc7906d992531de23cf6d48325a4e2
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-16 11:27:34 +0200

    CSS Overview

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -16,12 +16,20 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
           // more stuff coming up
-
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className='title'>{title}</p>
+              <p className="text">{quote}</p>
               <FaQuoteRight className='icon' />
             </article>
           );
@@ -29,7 +37,7 @@ function App() {
         <button className='prev'>
           <FiChevronLeft />
         </button>
-        <button className='prev'>
+        <button className='next'>
           <FiChevronRight />
         </button>
       </div>
