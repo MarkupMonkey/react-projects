aa902289d3c886143f1ec2f4965f361437fabee4
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-16 17:28:51 +0200

    Single Color

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -12,7 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10)
-      console.log(colors)
+      setList(colors)
 
     } catch (error) {
       setError(true)
@@ -25,7 +25,8 @@ function App() {
       <section className='container'>
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type='text' value={color} onChange={(e) => setColor(e.target.value)}
+          <input type='text' value={color}
+            onChange={(e) => setColor(e.target.value)}
             placeholder='#f15025' className={`${error ? 'error' : null}`} />
           <button className='btn' type='submit'>
             submit
@@ -33,7 +34,15 @@ function App() {
         </form>
       </section>
       <section className='colors'>
-        <h4>list foes here</h4>
+        {list.map((color, index) => {
+          console.log(color)
+          return <SingleColor
+            key={index}
+            {...color}
+            index={index}
+            hexColor={color.hex}
+          />
+        })}
       </section>
     </>
   )
