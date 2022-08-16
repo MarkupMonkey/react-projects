55cb77bf3360f40f97ab57f3ed744af250b843e7
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-16 18:18:36 +0200

    Add Items

--- 10-grocery-bud/setup/src/App.js
+++ 10-grocery-bud/setup/src/App.js
@@ -12,8 +12,18 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    if (!name) {
+      // display alert
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem])
+      setName('')
+    }
   }
+
   return <section className='section-center'>
     <form className='grocery-form' onSubmit={handleSubmit}>
       {alert.show && <Alert />}
@@ -27,12 +37,15 @@ function App() {
         </button>
       </div>
     </form>
-    <div className='grocery-container'>
-      <List />
-      <button className='clear-btn'>
-        clear item
-      </button>
-    </div>
+    {list.length > 0 && (
+      <div className='grocery-container'>
+        <List items={list} />
+        <button className='clear-btn'>
+          clear item
+        </button>
+      </div>
+    )}
+
   </section>
 }
 
