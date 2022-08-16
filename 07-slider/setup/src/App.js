8d8049cad1013d1aa79b01c2ac3b95dfab3d3a86
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-16 11:43:26 +0200

    Autoplay

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -2,9 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
+
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0)
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1)
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [index]);
+
   return (
     <section className='section'>
       <div className='title'>
@@ -16,6 +35,7 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
           // more stuff coming up
+
           let position = 'nextSlide';
           if (personIndex === index) {
             position = 'activeSlide';
@@ -34,6 +54,7 @@ function App() {
             </article>
           );
         })}
+
         <button className='prev' onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
