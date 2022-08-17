ce12521f6d11c63b277935488534091fd8c1ff71
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-17 16:00:26 +0200

    Links

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -7,18 +7,23 @@ export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
     const [location, setLocation] = useState({});
+    const [page, setPage] = useState({ page: '', links: [] })
 
     const openSidebar = () => {
         setIsSidebarOpen(true);
     }
+
+    const openSubmenu = (text, coordinates) => {
+        const page = sublinks.find((link) => link.page === text);
+        setPage(page)
+        setLocation(coordinates);
+        setIsSubmenuOpen(true);
+    }
+
     const closeSidebar = () => {
         setIsSidebarOpen(false);
     }
 
-    const openSubmenu = (text, coordinates) => {
-        setLocation(coordinates);
-        setIsSubmenuOpen(true);
-    }
 
     const closeSubmenu = () => {
         setIsSubmenuOpen(false);
@@ -33,7 +38,8 @@ export const AppProvider = ({ children }) => {
             openSidebar,
             closeSubmenu,
             closeSidebar,
-            location
+            location,
+            page,
         }}>
             {children}
         </AppContext.Provider>
