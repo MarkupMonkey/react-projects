ae71f6ecded77a0c19a0f123f6057c7a9f3ecb5f
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-17 12:48:09 +0200

    Context

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,16 @@
 import React, { useState, useContext } from 'react'
+
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    return <AppContext.Provider value='hello'>
+        {children}
+    </AppContext.Provider>
+}
+// custom hook require 'use' before NameOfHook
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
+
+export { AppContext, AppProvider }
