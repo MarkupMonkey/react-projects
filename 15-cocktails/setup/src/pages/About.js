3c032ec9327e9688d230892e1dcd06de2d1fcdff
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-18 08:59:58 +0200

    About and Error

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum maxime debitis dolores facilis voluptatem quasi dolorem dolorum maiores cum perspiciatis consequatur at facere sunt tenetur pariatur, laboriosam nobis ipsum! In atque, perferendis facere quis possimus quisquam neque expedita illum debitis!</p>
+    </section>
   )
 }
 
